d0439efd72453d45a50fc9faca56d7d9b96586e4
CaoYuanpu <yuanpucao@gmail.com>
2023-09-02 12:23:55 -0400

    test

--- test.py
+++ test.py
@@ -86,7 +86,7 @@ def main(
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
 
-    def evaluate(
+    def evaluate_new(
         instruction,
         input=None,
         temperature=0.1,
@@ -116,37 +116,6 @@ def main(
             "output_scores": True,
             "max_new_tokens": max_new_tokens,
         }
-
-        if stream_output:
-            # Stream the reply 1 token at a time.
-            # This is based on the trick of using 'stopping_criteria' to create an iterator,
-            # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
-
-            def generate_with_callback(callback=None, **kwargs):
-                kwargs.setdefault(
-                    "stopping_criteria", transformers.StoppingCriteriaList()
-                )
-                kwargs["stopping_criteria"].append(
-                    Stream(callback_func=callback)
-                )
-                with torch.no_grad():
-                    model.generate(**kwargs)
-
-            def generate_with_streaming(**kwargs):
-                return Iteratorize(
-                    generate_with_callback, kwargs, callback=None
-                )
-
-            with generate_with_streaming(**generate_params) as generator:
-                for output in generator:
-                    # new_tokens = len(output) - len(input_ids[0])
-                    decoded_output = tokenizer.decode(output)
-
-                    if output[-1] in [tokenizer.eos_token_id]:
-                        break
-
-                    yield prompter.get_response(decoded_output)
-            return  # early return for stream_output
         print("======= Stream_output: False=========")
         # Without streaming
         with torch.no_grad():
@@ -178,7 +147,8 @@ def main(
         "Count up from 1 to 500.",
     ]:
         print("Instruction:", instruction)
-        res = evaluate(instruction)
+        print('evaluate_new:')
+        res = evaluate_new(instruction)
         print("Response:", res)
         print()
 
